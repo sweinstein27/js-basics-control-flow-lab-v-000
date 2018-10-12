42db765e43239e9531f1963196981b3a2225120c
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 16:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-  if (someValue <= 400) {
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
     return 'This one is on me!';
-  }
-  else if (someValue > 2500) {
-    return 'No can do.'
-  }
-  else if (someValue > 2000) {
-    return 'I will gladly take your thirty bucks.';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
 }
 
-  // function ternaryCheckCity(city) {
-  //   return('NYC' ? "Ok, sounds good." : "No go.");
-  // }
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
