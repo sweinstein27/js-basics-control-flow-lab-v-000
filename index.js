53d0a2cc641fc4882ffbb56b0d0123e4c1293d37
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 15:59:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ function scuberGreetingForFeet(someValue) {
   }
 }
 
-function ternaryCheckCity 'NYC' ? "Ok, sounds good." : "No go."
+// function ternaryCheckCity 'NYC' ? "Ok, sounds good." : "No go."
