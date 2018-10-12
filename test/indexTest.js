30c09b8f582684d410ff980ad4bb499871a66a9a
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 16:13:36 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,20 +1,20 @@
-// const expect = chai.expect;
-// 
-// describe('index.js', function () {
-//   describe('scuberGreetingForFeet()', function () {
-//     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-//       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-//     });
-// 
-//     it('charges 30 dollars for a distance over 2000 feet', function () {
-//       expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-//     });
-// 
-//     it('does not allow rides over 2500 feet', function () {
-//       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-//     });
-//   });
-// 
+const expect = chai.expect;
+
+describe('index.js', function () {
+  describe('scuberGreetingForFeet()', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
+
+    it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
+
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
+
 //   describe('ternaryCheckCity()', function () {
 //     it('returns "Ok, sounds good." when the city is NYC', function () {
 //       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
