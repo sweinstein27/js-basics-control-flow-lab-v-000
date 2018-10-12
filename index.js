7ff4863efba19405ca8f9405cf3fe69541baba2a
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 15:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue) {
     return 'This one is on me!';
   }
   else if (someValue > 2500) {
-    return 'No can do'
+    return 'No can do.'
   }
   else if (someValue > 2000) {
     return 'I will gladly take your thirty bucks.';
