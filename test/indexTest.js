d5a1c1bec4e7dec3e8c823a1fbd9f73dfa562870
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 16:15:05 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -15,27 +15,27 @@ describe('index.js', function () {
     });
   });
 
-//   describe('ternaryCheckCity()', function () {
-//     it('returns "Ok, sounds good." when the city is NYC', function () {
-//       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-//     });
-//
-//     it('should return "No go." if the destination city is not NYC', function () {
-//       expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-//     });
-//   });
-//
-//   describe('switchOnCharmFromTip()', function () {
-//     it('should return "Thank you so much." if the tip is generous', function () {
-//       expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
-//     });
-//
-//     it('should return "Thank you." if the tip is not as generous', function () {
-//       expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
-//     });
-//
-//     it('should return "Bye." if anything else', function () {
-//       expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
-//     });
-//   });
-// });
+  describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
+
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });
+
+  describe('switchOnCharmFromTip()', function () {
+    it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
+
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
+
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    });
+  });
+});
